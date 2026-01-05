a6a2dd7fc773a0bce9f1332858f27b55bfc1e99b
fullstakdeveloper <pupru.edu2k7@gmail.com>
2026-01-05 17:17:14 -0500

    wrote some more backend functions

--- backend/src/main/java/com/example/demo/service/PlaidService.java
+++ backend/src/main/java/com/example/demo/service/PlaidService.java
@@ -3,12 +3,17 @@ package com.example.demo.service;
 import com.plaid.client.request.PlaidApi;
 import com.plaid.client.model.ItemPublicTokenExchangeRequest;
 import com.plaid.client.model.ItemPublicTokenExchangeResponse;
+import com.plaid.client.model.LinkTokenCreateRequestUser;
+import com.plaid.client.model.Products;
+import com.plaid.client.model.LinkTokenCreateRequest;
+import com.plaid.client.model.LinkTokenCreateResponse;
 
 import java.io.IOException;
 
 import org.springframework.stereotype.Service;
 import retrofit2.Response;
 
+import java.util.Arrays;
 
 @Service
 public class PlaidService {
@@ -26,6 +31,20 @@ public class PlaidService {
         //this stores the reponse that the plaid server sends
         Response<ItemPublicTokenExchangeResponse> response = plaidApi.itemPublicTokenExchange(request).execute();
 
+    }
+
+    public String createLinkToken(String userId) {
+        //created object for user
+        LinkTokenCreateRequestUser user = new LinkTokenCreateRequestUser().clientUserId(userId);
+
+        //created request with user object
+        LinkTokenCreateRequest request = new LinkTokenCreateRequest().user(user).clientName("Syn-napse").products(Arrays.asList(Products.TRANSACTIONS));
+
+        //executed request
+        Response<LinkTokenCreateResponse> response = plaidApi.linkTokenCreate(request).execute();  
+        
+        //returning value
+        return response.body().getLinkToken();
     }
 
     
